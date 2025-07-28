ed3a65effa1797415e42bb3a47e26bb9b69cc6b2
Angel Talero <angelgotalero@outlook.com>
2025-07-28 16:20:39 -0500

    Added LDA, LDX, LDY, STA, STX and STY

--- src/cpu/decode.c
+++ src/cpu/decode.c
@@ -17,14 +17,13 @@
 #include <stdbool.h>
 
 /* Private Functions */
-static inline nesemu_error_t
-_LDA(struct nes_cpu_t *self, uint8_t opcode, nes_memory_t mem, int *cycles)
+static inline nesemu_error_t _LDA(struct nes_cpu_t *self,
+				  uint8_t opcode,
+				  nes_memory_t mem,
+				  int *cycles)
 {
 	// Error code
 	nesemu_error_t err = NESEMU_RETURN_SUCCESS;
-
-	// Set cycles using the instruction
-	*cycles = nes_cpu_op_cycles[opcode];
 
 	// Placeholders for storing addresses
 	uint8_t lsb = 0, msb = 0;
@@ -90,7 +89,7 @@ _LDA(struct nes_cpu_t *self, uint8_t opcode, nes_memory_t mem, int *cycles)
 		break;
 
 	case LDA_AY:
-		// Load Accumulator Absolute, X
+		// Load Accumulator Absolute, Y
 		// Load contents at memory address and add Y
 		lsb = nes_cpu_fetch(self, mem), msb = nes_cpu_fetch(self, mem);
 		addr = NESEMU_UTIL_U16(msb, lsb) + self->y;
@@ -143,10 +142,335 @@ _LDA(struct nes_cpu_t *self, uint8_t opcode, nes_memory_t mem, int *cycles)
 	return err;
 }
 
+static inline nesemu_error_t _LDX(struct nes_cpu_t *self,
+				  uint8_t opcode,
+				  nes_memory_t mem,
+				  int *cycles)
+{
+	// Error code
+	nesemu_error_t err = NESEMU_RETURN_SUCCESS;
+
+	// Placeholders for storing addresses
+	uint8_t lsb = 0, msb = 0;
+
+	// Placeholder for a full address and pointer for indirect mode
+	uint16_t addr = 0, ptr = 0;
+
+	switch (opcode) {
+	case LDX_IM:
+		// Load X Immediate
+		self->x = nes_cpu_fetch(self, mem);
+		// Update status flags
+		nes_cpu_status_mask_set(self,
+					NESEMU_CPU_STATUS_MASK_NZ(self->x));
+		break;
+
+	case LDX_ZP:
+		// Load X Zero Page
+		// Read contents at addr at zero page
+		err = nes_mem_r8(
+			mem, NESEMU_ZEROPAGE_GET_ADDR(nes_cpu_fetch(self, mem)),
+			&self->x);
+		// Update status flags
+		nes_cpu_status_mask_set(self,
+					NESEMU_CPU_STATUS_MASK_NZ(self->x));
+		break;
+
+	case LDX_ZY:
+		// Load Y Zero Page and add Y reegistere
+		// Read contents at addr at zero page
+		err = nes_mem_r8(mem,
+				 NESEMU_ZEROPAGE_GET_ADDR(
+					 nes_cpu_fetch(self, mem) + self->y),
+				 &self->x);
+		// Update status flags
+		nes_cpu_status_mask_set(self,
+					NESEMU_CPU_STATUS_MASK_NZ(self->x));
+		break;
+
+	case LDX_AB:
+		// Load X Absolute
+		// Load contents at memory address
+		lsb = nes_cpu_fetch(self, mem), msb = nes_cpu_fetch(self, mem);
+		err = nes_mem_r8(mem, NESEMU_UTIL_U16(msb, lsb), &self->x);
+		// Update status flags
+		nes_cpu_status_mask_set(self,
+					NESEMU_CPU_STATUS_MASK_NZ(self->x));
+		break;
+
+	case LDX_AY:
+		// Load X Absolute, Y
+		// Load contents at memory address and add Y
+		lsb = nes_cpu_fetch(self, mem), msb = nes_cpu_fetch(self, mem);
+		addr = NESEMU_UTIL_U16(msb, lsb) + self->y;
+		err = nes_mem_r8(mem, addr, &self->x);
+		// Update status flags
+		nes_cpu_status_mask_set(self,
+					NESEMU_CPU_STATUS_MASK_NZ(self->x));
+		// Add one cycle if cross pagee
+		if (nes_mem_is_crosspage(addr)) {
+			*cycles += 1;
+		}
+		break;
+
+	default:
+		return NESEMU_RETURN_CPU_UNSUPPORTED_INSTRUCTION;
+	}
+
+	return err;
+}
+
+static inline nesemu_error_t _LDY(struct nes_cpu_t *self,
+				  uint8_t opcode,
+				  nes_memory_t mem,
+				  int *cycles)
+{
+	// Error code
+	nesemu_error_t err = NESEMU_RETURN_SUCCESS;
+
+	// Placeholders for storing addresses
+	uint8_t lsb = 0, msb = 0;
+
+	// Placeholder for a full address and pointer for indirect mode
+	uint16_t addr = 0, ptr = 0;
+
+	switch (opcode) {
+	case LDY_IM:
+		// Load Y Immediate
+		self->y = nes_cpu_fetch(self, mem);
+		// Update status flags
+		nes_cpu_status_mask_set(self,
+					NESEMU_CPU_STATUS_MASK_NZ(self->y));
+		break;
+
+	case LDY_ZP:
+		// Load Y Zero Page
+		// Read contents at addr at zero page
+		err = nes_mem_r8(
+			mem, NESEMU_ZEROPAGE_GET_ADDR(nes_cpu_fetch(self, mem)),
+			&self->y);
+		// Update status flags
+		nes_cpu_status_mask_set(self,
+					NESEMU_CPU_STATUS_MASK_NZ(self->y));
+		break;
+
+	case LDY_ZX:
+		// Load X Zero Page and add X register
+		// Read contents at addr at zero page
+		err = nes_mem_r8(mem,
+				 NESEMU_ZEROPAGE_GET_ADDR(
+					 nes_cpu_fetch(self, mem) + self->x),
+				 &self->y);
+		// Update status flags
+		nes_cpu_status_mask_set(self,
+					NESEMU_CPU_STATUS_MASK_NZ(self->y));
+		break;
+
+	case LDY_AB:
+		// Load Y Absolute
+		// Load contents at memory address
+		lsb = nes_cpu_fetch(self, mem), msb = nes_cpu_fetch(self, mem);
+		err = nes_mem_r8(mem, NESEMU_UTIL_U16(msb, lsb), &self->y);
+		// Update status flags
+		nes_cpu_status_mask_set(self,
+					NESEMU_CPU_STATUS_MASK_NZ(self->y));
+		break;
+
+	case LDY_AX:
+		// Load Y Absolute, X
+		// Load contents at memory address and add X
+		lsb = nes_cpu_fetch(self, mem), msb = nes_cpu_fetch(self, mem);
+		addr = NESEMU_UTIL_U16(msb, lsb) + self->x;
+		err = nes_mem_r8(mem, addr, &self->y);
+		// Update status flags
+		nes_cpu_status_mask_set(self,
+					NESEMU_CPU_STATUS_MASK_NZ(self->y));
+		// Add one cycle if cross pagee
+		if (nes_mem_is_crosspage(addr)) {
+			*cycles += 1;
+		}
+		break;
+
+	default:
+		return NESEMU_RETURN_CPU_UNSUPPORTED_INSTRUCTION;
+	}
+
+	return err;
+}
+
+static inline nesemu_error_t _STA(struct nes_cpu_t *self,
+				  uint8_t opcode,
+				  nes_memory_t mem,
+				  int *cycles)
+{
+	// Error code
+	nesemu_error_t err = NESEMU_RETURN_SUCCESS;
+
+	// Placeholders for storing addresses
+	uint8_t lsb = 0, msb = 0;
+
+	// Placeholder for a full address and pointer for indirect mode
+	uint16_t addr = 0, ptr = 0;
+
+	switch (opcode) {
+	case STA_ZP:
+		// Store Accumulator, Zero Page
+		addr = NESEMU_ZEROPAGE_GET_ADDR(nes_cpu_fetch(self, mem));
+		err = nes_mem_w8(mem, addr, self->a);
+		break;
+
+	case STA_ZX:
+		// Store Accumulator, Zero Page + X
+		addr = NESEMU_ZEROPAGE_GET_ADDR(nes_cpu_fetch(self, mem)) +
+		       self->x;
+		err = nes_mem_w8(mem, addr, self->a);
+		break;
+
+	case STA_AB:
+		// Store Accumulator, Absolute
+		lsb = nes_cpu_fetch(self, mem), msb = nes_cpu_fetch(self, mem);
+		addr = NESEMU_UTIL_U16(msb, lsb);
+		err = nes_mem_w8(mem, addr, self->a);
+		break;
+
+	case STA_AX:
+		// Store Accumulator, Absolute + X
+		lsb = nes_cpu_fetch(self, mem), msb = nes_cpu_fetch(self, mem);
+		addr = NESEMU_UTIL_U16(msb, lsb) + self->x;
+		err = nes_mem_w8(mem, addr, self->a);
+		break;
+
+	case STA_AY:
+		// Store Accumulator, Absolute + Y
+		lsb = nes_cpu_fetch(self, mem), msb = nes_cpu_fetch(self, mem);
+		addr = NESEMU_UTIL_U16(msb, lsb) + self->y;
+		err = nes_mem_w8(mem, addr, self->a);
+		break;
+
+	case STA_IX:
+		// Store Accumulator, Indirect X (Pre-Indexed)
+		// Load the pointer address (always zero page) and add X
+		ptr = NESEMU_ZEROPAGE_GET_ADDR(nes_cpu_fetch(self, mem) +
+					       self->x);
+		// Contents in ptr address will be the actual address
+		err = nes_mem_r16(mem, ptr, &addr);
+		if (err != NESEMU_RETURN_SUCCESS) {
+			return err;
+		}
+		// Write $acc into actual address
+		err = nes_mem_w8(mem, addr, self->a);
+		break;
+
+	case STA_IY:
+		// Store Accumulator, Indirect Y (Post-Indexed)
+		// Load the pointer address (always zero page)
+		ptr = NESEMU_ZEROPAGE_GET_ADDR(nes_cpu_fetch(self, mem));
+		// Contents in ptr address will be the actual address
+		err = nes_mem_r16(mem, ptr, &addr);
+		if (err != NESEMU_RETURN_SUCCESS) {
+			return err;
+		}
+		// Add Y to the actual address
+		addr += self->y;
+		// Write $acc into actual address
+		err = nes_mem_w8(mem, addr, self->a);
+		break;
+	default:
+		return NESEMU_RETURN_CPU_UNSUPPORTED_INSTRUCTION;
+	}
+
+	return err;
+}
+
+static inline nesemu_error_t _STX(struct nes_cpu_t *self,
+				  uint8_t opcode,
+				  nes_memory_t mem,
+				  int *cycles)
+{
+	// Error code
+	nesemu_error_t err = NESEMU_RETURN_SUCCESS;
+
+	// Placeholders for storing addresses
+	uint8_t lsb = 0, msb = 0;
+
+	// Placeholder for a full address and pointer for indirect mode
+	uint16_t addr = 0, ptr = 0;
+
+	switch (opcode) {
+	case STX_ZP:
+		// Store X, Zero Page
+		addr = NESEMU_ZEROPAGE_GET_ADDR(nes_cpu_fetch(self, mem));
+		err = nes_mem_w8(mem, addr, self->x);
+		break;
+
+	case STX_ZY:
+		// Store X, Zero Page + Y
+		addr = NESEMU_ZEROPAGE_GET_ADDR(nes_cpu_fetch(self, mem)) +
+		       self->y;
+		err = nes_mem_w8(mem, addr, self->x);
+		break;
+
+	case STX_AB:
+		// Store X, Absolute
+		lsb = nes_cpu_fetch(self, mem), msb = nes_cpu_fetch(self, mem);
+		addr = NESEMU_UTIL_U16(msb, lsb);
+		err = nes_mem_w8(mem, addr, self->x);
+		break;
+
+	default:
+		return NESEMU_RETURN_CPU_UNSUPPORTED_INSTRUCTION;
+	}
+
+	return err;
+}
+
+static inline nesemu_error_t _STY(struct nes_cpu_t *self,
+				  uint8_t opcode,
+				  nes_memory_t mem,
+				  int *cycles)
+{
+	// Error code
+	nesemu_error_t err = NESEMU_RETURN_SUCCESS;
+
+	// Placeholders for storing addresses
+	uint8_t lsb = 0, msb = 0;
+
+	// Placeholder for a full address and pointer for indirect mode
+	uint16_t addr = 0, ptr = 0;
+
+	switch (opcode) {
+	case STY_ZP:
+		// Store Y, Zero Page
+		addr = NESEMU_ZEROPAGE_GET_ADDR(nes_cpu_fetch(self, mem));
+		err = nes_mem_w8(mem, addr, self->y);
+		break;
+
+	case STY_ZX:
+		// Store Y, Zero Page + X
+		addr = NESEMU_ZEROPAGE_GET_ADDR(nes_cpu_fetch(self, mem)) +
+		       self->x;
+		err = nes_mem_w8(mem, addr, self->y);
+		break;
+
+	case STY_AB:
+		// Store Y, Absolute
+		lsb = nes_cpu_fetch(self, mem), msb = nes_cpu_fetch(self, mem);
+		addr = NESEMU_UTIL_U16(msb, lsb);
+		err = nes_mem_w8(mem, addr, self->y);
+		break;
+
+	default:
+		return NESEMU_RETURN_CPU_UNSUPPORTED_INSTRUCTION;
+	}
+
+	return err;
+}
+
 /* Public Functions */
 
-nesemu_error_t
-nes_cpu_next(struct nes_cpu_t *self, nes_memory_t mem, int *cycles)
+nesemu_error_t nes_cpu_next(struct nes_cpu_t *self,
+			    nes_memory_t mem,
+			    int *cycles)
 {
 	/* Check input arguments */
 #ifndef CONFIG_NESEMU_DISABLE_SAFETY_CHECKS
@@ -172,21 +496,14 @@ nes_cpu_next(struct nes_cpu_t *self, nes_memory_t mem, int *cycles)
 	switch (opcode) {
 	/* Load/Store Operations */
 	case LDA_IM:
-        __attribute__((fallthrough));
 	case LDA_ZP:
-        __attribute__((fallthrough));
 	case LDA_ZX:
-        __attribute__((fallthrough));
 	case LDA_AB:
-        __attribute__((fallthrough));
 	case LDA_AX:
-        __attribute__((fallthrough));
 	case LDA_AY:
-        __attribute__((fallthrough));
 	case LDA_IX:
-        __attribute__((fallthrough));
 	case LDA_IY:
-        err = _LDA(self, opcode, mem, cycles);
+		err = _LDA(self, opcode, mem, cycles);
 		break;
 
 	case LDX_IM:
@@ -194,12 +511,16 @@ nes_cpu_next(struct nes_cpu_t *self, nes_memory_t mem, int *cycles)
 	case LDX_ZY:
 	case LDX_AB:
 	case LDX_AY:
+		err = _LDX(self, opcode, mem, cycles);
+		break;
 
 	case LDY_IM:
 	case LDY_ZP:
 	case LDY_ZX:
 	case LDY_AB:
 	case LDY_AX:
+		err = _LDY(self, opcode, mem, cycles);
+		break;
 
 	case STA_ZP:
 	case STA_ZX:
@@ -208,14 +529,20 @@ nes_cpu_next(struct nes_cpu_t *self, nes_memory_t mem, int *cycles)
 	case STA_AY:
 	case STA_IX:
 	case STA_IY:
+		err = _STA(self, opcode, mem, cycles);
+		break;
 
 	case STX_ZP:
 	case STX_ZY:
 	case STX_AB:
+		err = _STX(self, opcode, mem, cycles);
+		break;
 
 	case STY_ZP:
 	case STY_ZX:
 	case STY_AB:
+		err = _STY(self, opcode, mem, cycles);
+		break;
 
 		/* Register Transfers */
 	case TAX:
